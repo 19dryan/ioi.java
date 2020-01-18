e7be61bd107188eb471b9a3c05cd5eaa31d965a4
Daniel Ryan <Daniel Ryan@DESKTOP-S87R8U2>
2020-01-18 15:38:05 +0000

    v1

---
+++ src/ioi/java/ApplicationMain.java
@@ -0,0 +1,47 @@
+/*
+ To change this license header, choose License Headers in Project Properties.
+ To change this template file, choose Tools | Templates
+ and open the template in the editor.
+ */
+package ioi.java;
+
+/**
+ *
+ * @author Conchraidh
+ */
+public class ApplicationMain {
+
+    public static void main(String[] args) {
+        String name="Daniel";
+        int age=13;
+        int number1=10;
+        int number2=5;
+        
+        System.out.println(name);
+        System.out.println(age);
+        
+        int answer=number1*number2;
+        
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(answer);
+        
+        System.out.println("My name is " + name + "My age is" + age + "My answer is" + answer);
+        /**
+         * @param args the command line arguments
+         */
+    }
+   
+//        int counter = 1;
+//        while (counter < 5) {
+//            System.out.println("Number: " + counter);
+//            counter = counter + 1;
+//        }
+        //System.out.println("1");
+        //System.out.println("2");
+        //System.out.println("3");
+        //System.out.println("4");
+        // TODO code application logic here
+    }
+
+
